bdb111553f41026cd2ed62a2d8dcac5e9b820ace
changsun <kcdpads321>
2022-04-06 14:43:23 +0900

    Updates

---
+++ precache-manifest.ef88a41e4431d26c8118e189068e8faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33323430d15a1e0b33bd0f72f2771bf7",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "a45e2cbd583f0b7d9129",
+    "url": "/habit_tracker/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "30ea2229acc1adba22ad",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.e8da4bea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a45e2cbd583f0b7d9129",
+    "url": "/habit_tracker/static/js/main.4154bae9.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
